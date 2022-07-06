c9675a8d7c0f83384d138d4044ea4fe2bbc75410
Ingo Knorr <ingoknorr@gmail.com>
2022-07-06 17:31:55 +0200

    refactured fetch data. added that icons get generated properly

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Setting variables
+
 // https://api.openweathermap.org/data/2.5/weather?q=London&appid=4739904bd0e17a0b10ab5e88b48f19b7
 const API = '4739904bd0e17a0b10ab5e88b48f19b7';
 const input = document.querySelector('input');
@@ -6,52 +8,65 @@ const cityName = document.querySelector('.city-name');
 const country = document.querySelector('.country');
 const area = document.querySelector('.area');
 const temperature = document.querySelector('.temperature');
+const humidityIcon = document.querySelector('.humidity-icon');
 const humidity = document.querySelector('.humidity');
+const windIcon = document.querySelector('.wind-icon');
 const wind = document.querySelector('.wind');
+const cloudsIcon = document.querySelector('.clouds-icon');
 const clouds = document.querySelector('.clouds');
 const icons = document.querySelectorAll('.icons');
 const weatherIcon = document.querySelector('#weather-icon');
 const weatherMain = document.querySelector('.weather-main');
 const locationBtn = document.querySelector('.location');
 
+// Get data function
+const getData = function (data) {
+  humidityIcon.innerHTML = '<i class="icons fa-solid fa-droplet"></i>';
+  humidity.innerText = `${data.main.humidity}%`;
+  windIcon.innerHTML = '<i class="icons fa-solid fa-wind"></i>';
+  wind.innerText = `${data.wind.speed}m/s`;
+  cloudsIcon.innerHTML = '<i class="icons fa-solid fa-cloud"></i>';
+  clouds.innerText = `${data.clouds.all}%`;
+
+  cityName.innerText = `${data.name},${data.sys.country}`;
+
+  // const tempKelvin = data.main.temp;
+  // const tempCelsius = parseInt(tempKelvin - 273.15);
+  // temperature.innerText = `${tempCelsius}°`;
+  temperature.innerText = `${parseInt(data.main.temp)}°`;
+
+  weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  weatherMain.innerText = data.weather[0].description;
+};
+
+// Calling API / displaying content
 searchBtn.addEventListener('click', function (e) {
   e.preventDefault();
   const searchInput = input.value;
 
   const getWeatherData = async function () {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${API}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${API}&units=metric`
     );
     const data = await response.json();
     console.log(data);
 
-    icons.forEach(icon => {
-      icon.classList.remove('hidden');
-    });
+    // icons.forEach(icon => {
+    //   icon.classList.remove('hidden');
+    // });
 
-    humidity.innerText = `${data.main.humidity}%`;
-    wind.innerText = `${data.wind.speed}m/s`;
-    clouds.innerText = `${data.clouds.all}%`;
-
-    cityName.innerText = `${data.name},${data.sys.country}`;
-
-    const tempKelvin = data.main.temp;
-    const tempCelsius = parseInt(tempKelvin - 273.15);
-    temperature.innerText = `${tempCelsius}°`;
-
-    weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    weatherMain.innerText = data.weather[0].description;
+    getData(data);
   };
   getWeatherData();
 
   input.value = '';
 });
 
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
+// get local weather on page load
 let lat;
 let lon;
 
+// Geolocation
 const success = function (pos) {
   const position = pos.coords;
   lat = position.latitude;
@@ -59,35 +74,25 @@ const success = function (pos) {
 };
 
 const error = () => {
-  console.log('Unable to find your location');
+  cityName.innerText = 'Unable to find your location';
 };
 
 navigator.geolocation.getCurrentPosition(success, error);
 
+// On window load display local weather
 window.addEventListener('load', () => {
   const getMyLocationWeather = async function () {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}&units=metric`
     );
     const data = await response.json();
     console.log(data);
 
-    icons.forEach(icon => {
-      icon.classList.remove('hidden');
-    });
+    // icons.forEach(icon => {
+    //   icon.classList.remove('hidden');
+    // });
 
-    humidity.innerText = `${data.main.humidity}%`;
-    wind.innerText = `${data.wind.speed}m/s`;
-    clouds.innerText = `${data.clouds.all}%`;
-
-    cityName.innerText = `${data.name},${data.sys.country}`;
-
-    const tempKelvin = data.main.temp;
-    const tempCelsius = parseInt(tempKelvin - 273.15);
-    temperature.innerText = `${tempCelsius}°`;
-
-    weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    weatherMain.innerText = data.weather[0].description;
+    getData(data);
   };
   getMyLocationWeather();
 });
